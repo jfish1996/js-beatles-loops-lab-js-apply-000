5ae6a13d208a71fe910d8aa2e4e082a5ddaaf539
John Traut <jtraut1996@gmail.com>
2018-11-19 19:03:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(part1, part2){
+var empty = [];
+  for (var counter = 0; counter < part1.length; counter++){
+    empty.push(part1[counter] + " plays " + part2[counter]);
+  }
+  return empty;
+}
+
+function johnLennonFacts(arg){
+var counter = 0
+  while (counter < arg.length ){
+  arg[counter] += "!!!";
+  counter++;
+  }
+  return arg;
+}
